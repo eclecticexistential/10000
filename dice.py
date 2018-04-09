58ea4abd451206f002b78badf9d570d4342a620b
eclecticexistential <allofitbra@gmail.com>
2018-04-08 21:20:21 -0700

    Take turn function created. Tested dice roll to confirm score accuracy. Added number of dice as an input to make recurion easier.

--- dice.py
+++ dice.py
@@ -35,7 +35,7 @@ def count_singles(die):
         return 50
 
 
-def count_die(roll):
+def count_die(roll, num_die):
     print(roll)
     ones = roll.count(1)
     twos = roll.count(2)
@@ -44,61 +44,87 @@ def count_die(roll):
     fives = roll.count(5)
     sixes = roll.count(6)
     tots = [ones, twos, threes, fours, fives, sixes]
-    die = 6
+    die = num_die
     score = 0
+    num2 = tots.count(2)
+    num1 = tots.count(1)
+    if num2 == 3:
+        score += count_doubles()
+        die -= 6
+    for key, value in enumerate(tots):
+        pos = key + 1
+        if num1 > 3:
+            if key == 0 and value == 2:
+                if fives == 1:
+                    score += count_straight(5) + count_singles(5) * value
+                    die -= 6
+            if key == 4 and value == 2:
+                score += count_straight(5) + count_singles(5)
+                die -= 6
+        if value == 3:
+            score += count_triples(pos)
+            die -= 3
+        if value == 6:
+            score += (count_triples(pos)) * 2
+            die -= 6
+        if value == 4 and 2 in tots and key == 0:
+            score += (count_triples(pos) + count_singles(1))
+            die -= 4
+        if value == 4 and 2 in tots and key != 0:
+            score += count_doubles()
+            die -= 6
+        if value == 5:
+            if key == 4:
+                score += (count_triples(pos) + count_singles(5) * 2)
+                die -= 5
+            else:
+                score += count_triples(pos)
+                die -= 3
     if ones == twos == threes == fours == fives == sixes:
-        return count_straight(6), die
-    if ones > 0:
-        if twos == threes == fours == fives:
-            score = count_straight(5) + count_singles(1)
-            return score, die
-        if ones < 3:
-            score += count_singles(1) * ones
-            die -= ones
-        if ones > 3:
-            if ones == 6:
-                score += count_triples(1) * 2
-                return score, die
-            else:
-                score += count_singles(1) * (ones - 3)
-                die -= (ones - 3)
-    if 3 in tots:
-        for key, value in enumerate(tots):
-            if value == 3:
-                score += count_triples(key+1)
-                die -= 3
-    if 4 in tots:
-        for key, value in enumerate(tots):
-            if value == 4:
-                score += count_triples(key+1)
-                die -= 3
-    if fives > 0:
-        if ones == twos == threes == fours:
-            score = count_straight(5) + count_singles(5)
-            return score, die
-        if twos == threes == fours == sixes:
-            score = count_straight(5) + count_singles(5)
-            return score, die
-        if fives < 3:
-            score += count_singles(5) * fives
-            die -= fives
-        if fives > 3:
-            if fives == 6:
-                score += count_triples(5) * 2
-                return score, die
-            else:
-                score += count_singles(5) * (fives - 3)
-                die -= (fives - 3)
-    if 2 in tots:
-        num = tots.count(2)
-        if num == 3:
-            score = count_doubles()
-            return score, die
+        score += count_straight(6)
+        die -= 6
+    if die == 0:
+        return score, 6
+    if ones < 3 and die > 0:
+        score += count_singles(1) * ones
+        die -= ones
+    if fives < 3 and die > 0:
+        score += count_singles(5) * fives
+        die -= fives
     if score > 0:
         return score, die
-    else:
-        return roll
+    if score == 0:
+        return 0, 0
 
 
+def take_turn(on_the_board=False, gambler=False):
+    score = 0
+    if on_the_board is False:
+        stats = count_die(dice_roll(6), 6)
+        if stats[0] == 0:
+            print("busted")
+            return score
+        score += stats[0]
+        die = stats[1]
+        print(score, die)
+        while score < 1000:
+            new_roll = count_die(dice_roll(die), die)
+            if new_roll != (0, 0):
+                score += new_roll[0]
+                die = new_roll[1]
+                print(new_roll, score, die)
+                if die == 0:
+                    die = 6
+            elif new_roll == (0, 0):
+                print("boo")
+                return 0
+        if score > 999:
+            return score
 
-print(count_die(dice_roll(6)))
+    if gambler is False:
+        pass
+    elif gambler:
+        pass
+
+
+take_turn()
